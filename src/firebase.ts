fd670e5b78c6e02e8a418540a46d399b73b0ca63
ArkamRizwan <ArkamRizwan@github.com>
2022-05-13 11:10:15 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyB_DJ3-vWQuSzbMo_r3Uci7Mxfu4yENwIk',
-  authDomain: 'ijazah-tours-2.firebaseapp.com',
-  projectId: 'ijazah-tours-2',
-  storageBucket: 'ijazah-tours-2.appspot.com',
-  messagingSenderId: '588578726216',
-  appId: '1:588578726216:web:aedd95334d7493e36c537d',
+  apiKey: "AIzaSyB_DJ3-vWQuSzbMo_r3Uci7Mxfu4yENwIk",
+  authDomain: "ijazah-tours-2.firebaseapp.com",
+  projectId: "ijazah-tours-2",
+  storageBucket: "ijazah-tours-2.appspot.com",
+  messagingSenderId: "588578726216",
+  appId: "1:588578726216:web:aedd95334d7493e36c537d",
   measurementId: "G-CV10HJS6K9"
 };
 
-initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore();
-
-export { auth, db };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
