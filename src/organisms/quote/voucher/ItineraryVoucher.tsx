58c2137550943d5363d7e36fd85badbcabab7c4a
Arkam Rizwan <100221954+markamriz@users.noreply.github.com>
2023-05-15 19:26:46 +0530

    Update ItineraryVoucher.tsx

--- src/organisms/quote/voucher/ItineraryVoucher.tsx
+++ src/organisms/quote/voucher/ItineraryVoucher.tsx
@@ -48,7 +48,7 @@ function ItineraryVoucher({ voucherData, setIsVoucherApproved }: ItineraryVouche
 
   const generatePDF = async () => {
     const { elementWidth, elementHeight } = getElementWidth('report');
-    const report = new JSPDF('landscape', 'pt', [elementWidth, elementHeight]);
+    const report = new JSPDF('landscape', 'pt', [elementWidth + 10, elementHeight]);
     report.setFont('Arial');
 
     return report.html(document.querySelector('#report') as HTMLElement, {
